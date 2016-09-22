37332211e24df3c9336cc7051d6cc8afec6e689f
NKawaru <nagisakawaru@163.com>
2016-09-22 11:20:53 +0800

    Initial Commit

---
+++ 签名ViewTests/__ViewTests.m
@@ -0,0 +1,39 @@
+//
+//  __ViewTests.m
+//  签名ViewTests
+//
+//  Created by 楊利嘉 on 2016/9/22.
+//  Copyright © 2016年 楊利嘉. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __ViewTests : XCTestCase
+
+@end
+
+@implementation __ViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
